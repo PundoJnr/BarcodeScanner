4f377efc2df40e9349e0bef70ef4b56585c9ea87
ArthurPundo <pundoarthur@gmail.com>
2018-05-07 11:21:34 +0300

    deployed

--- app/src/main/java/com/example/poline/barcode/main.java
+++ app/src/main/java/com/example/poline/barcode/main.java
@@ -60,9 +60,9 @@ public class main extends AppCompatActivity {
            String resultCode = result.getText().toString();
             //Toast.makeText(main.this, "http://nyeri.leja.co.ke/success.php?ref=" +resultCode, Toast.LENGTH_LONG);
             scannerView.stopCamera();
-            String finalText = "http://dues.co.ke/confirm.php?txt-d_num=" +resultCode;
+            String finalText = "http://dues.co.ke/confirm.php?txtid_num=" +resultCode;
            // textView2.setText("http://nyeri.leja.co.ke/success.php?ref=" +resultCode);
-            String text = " <a href='http://nyeri.leja.co.ke/success.php?ref= " + resultCode +" '> View Profile</a>";
+            String text = " <a href='http://dues.co.ke/confirm.php?txtid_num=" + resultCode +" '> View Profile</a>";
 
             textView2.setText(Html.fromHtml(text));
 
